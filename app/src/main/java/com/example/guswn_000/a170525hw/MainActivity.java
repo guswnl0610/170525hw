5b0308bd9f9654014bc7ae80fbb7ba5d33c6042e
guswnl0610 <guswnl0610@naver.com>
2017-05-25 16:23:31 +0900

    loading

--- app/src/main/java/com/example/guswn_000/a170525hw/MainActivity.java
+++ app/src/main/java/com/example/guswn_000/a170525hw/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.guswn_000.a170525hw;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    ImageView imageView;
+    TextView textView;
+    EditText editText;
+    int[] images = {R.drawable.fork,R.drawable.okkono,R.drawable.pasta,R.drawable.pizza,R.drawable.ramen,R.drawable.steak};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        textView = (TextView)findViewById(R.id.tv);
+        editText = (EditText)findViewById(R.id.editText);
     }
+
+    class myThread extends AsyncTask<Integer,Integer,Void>
+    {
+
+        @Override
+        protected Void doInBackground(Integer... params)
+        {
+
+            return null;
+        }
+    }
+
+    public void onClick(View v)
+    {
+        
+    }
+
 }
